f3fc0ed96174bd44f1e9a366646e1c5f625f083a
ustczy <ustczy@users.noreply.github.com>
2023-07-18 12:28:02 +0800

    Update api.py

--- server/api.py
+++ server/api.py
@@ -10,7 +10,6 @@ DEVICE_ID = "0"
 CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-chatglm_model_path = os.path.join(current_dir, "THUDM/chatglm-6b")
 fine_tuning_model_path = os.path.join(current_dir, "../weights/api-fn")
 
 def torch_gc():
@@ -55,8 +54,8 @@ async def create_item(request: Request):
 
 
 if __name__ == '__main__':
-    tokenizer = AutoTokenizer.from_pretrained(chatglm_model_path, trust_remote_code=True)
-    model = AutoModel.from_pretrained(chatglm_model_path, trust_remote_code=True).half().cuda()
+    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+    model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
 
     # 加载微调模型
     model = PeftModel.from_pretrained(model, fine_tuning_model_path).half()
